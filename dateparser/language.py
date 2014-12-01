6c04d6f8ffff1de50390e7b2485375fe0fbaf666
Eugene Amirov <eugene.amirov@gmail.com>
2014-12-02 00:30:31 +0300

    Populating language info with base info

--- dateparser/language.py
+++ dateparser/language.py
@@ -7,18 +7,38 @@ from yaml import load as load_yaml
 
 
 class LanguageDataLoader(object):
+    _data = None
+
     def __init__(self, file=None):
         if isinstance(file, basestring):
             file = open(file)
         self.file = file
 
-    def load_data(self):
+    def get_languages(self):
+        if self._data is None:
+            self._load_data()
+        return self._data
+
+    def _load_data(self):
         if self.file is None:
             data = get_data('data', 'languages.yaml')
         else:
             data = self.file.read()
         data = load_yaml(data)
-        return data
+        base_data = data.pop('base', {})
+        known_languages = {}
+        for shortname, language_info in data.iteritems():
+            self._update_language_info_with_base_info(language_info, base_data)
+            language = Language(shortname, language_info)
+            if language.validate_info():
+                known_languages[shortname] = language
+        self._data = known_languages
+
+    def _update_language_info_with_base_info(self, language_info, base_info):
+        for key, values in base_info.iteritems():
+            if isinstance(values, list):
+                extended_values = (values + language_info[key]) if key in language_info else values
+                language_info[key] = extended_values
 
 
 class Language(object):
