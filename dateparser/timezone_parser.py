6615e90159502798f940c021f4425fcbfcbebaa5
Joseph Kahn <josephbkahn@gmail.com>
2014-12-02 13:00:19 -0500

    removing unnecessary code

--- dateparser/timezone_parser.py
+++ dateparser/timezone_parser.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
 from datetime import datetime, timedelta
-from math import ceil
 
 from dateparser.timezones import timezone_info_list
 
@@ -26,9 +25,7 @@ def convert_to_local_tz(datetime_obj, datetime_tz_offset):
 
 
 def get_local_tz_offset():
-    delta = datetime.now() - datetime.utcnow()
-    days, seconds, microseconds = delta.days, delta.seconds, delta.microseconds
-    return timedelta(seconds=days * 24 * HOUR + seconds + ceil(microseconds / 1000000.0))
+    return datetime.now() - datetime.utcnow()
 
 
 _tz_offsets = get_tz_offsets()
