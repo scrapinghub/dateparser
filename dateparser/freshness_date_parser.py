0c45521a4b312dd5882ed0c9dee4d548d2fc6cda
Waqas <waqas@scrapinghub.com>
2015-01-12 20:11:53 +0500

    Proposed fix for #22 Added tests

--- dateparser/freshness_date_parser.py
+++ dateparser/freshness_date_parser.py
@@ -5,6 +5,9 @@ import re
 from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
+from dateutil.parser import parse
+
+_UNITS = r'year|month|week|day|hour|minute|second'
 
 
 class FreshnessDateDataParser(object):
@@ -14,18 +17,36 @@ class FreshnessDateDataParser(object):
         self.now = now or datetime.utcnow()
 
     def _are_all_words_units(self, date_string):
-        skip = [
-            r'year|month|week|day|hour|minute|second',  # units
-            r'about|ago|\d+',  # others
-        ]
+        skip = [_UNITS,
+                r'about|ago|\d+',
+                r':|[ap]m'] 
 
         date_string = re.sub(r'\s+', ' ', date_string.strip())
 
         words = filter(lambda x: x if x else False, re.split('\W', date_string))
-        words = filter(lambda x: not re.match('|'.join(skip), x), words)
+        words = filter(lambda x: not re.match(r'%s' % '|'.join(skip), x), words)
         return not bool(words)
 
+    def _parse_time(self, date_string):
+        date_string = re.sub(r'\d+\s*(%s)' % _UNITS, '', date_string)
+        date_string = re.sub(r'[\.,]', '', date_string)
+        try:
+            return parse(date_string).time()
+        except:
+            pass
+
     def parse(self, date_string):
+        time = self._parse_time(date_string)
+
+        date, period = self._parse(date_string)
+        
+        if date and time:
+            date = date.replace(hour=time.hour, minute=time.minute)
+
+        return date, period
+
+
+    def _parse(self, date_string):
         if not self._are_all_words_units(date_string):
             return None, None
 
@@ -45,7 +66,7 @@ class FreshnessDateDataParser(object):
         return date, period
 
     def get_kwargs(self, date_string):
-        m = re.findall(r'(\d+)\s*(year|month|week|day|hour|minute|second)\b', date_string, re.I | re.S | re.U)
+        m = re.findall(r'(\d+)\s*(%s)\b' % _UNITS, date_string, re.I | re.S | re.U)
         if not m:
             return {}
 
--- tests/test_freshness_date_parser.py
+++ tests/test_freshness_date_parser.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 import re
 import unittest
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date, time
 from functools import wraps
 
 from dateutil.relativedelta import relativedelta
@@ -24,6 +24,8 @@ class TestFreshnessDateDataParser(BaseTestCase):
         self.freshness_parser = NotImplemented
         self.freshness_result = NotImplemented
         self.exception = NotImplemented
+        self.date = NotImplemented
+        self.time = NotImplemented
 
     @parameterized.expand([
         # English dates
@@ -259,6 +261,20 @@ class TestFreshnessDateDataParser(BaseTestCase):
         self.then_period_is(period)
         self.then_date_obj_is_between(self.now - timedelta(**boundary), self.now)
 
+    @parameterized.expand([
+        param('Today at 9 pm', date(2014, 9, 1), time(21, 0)),
+        param('Today at 11:20 am', date(2014, 9, 1), time(11, 20)),
+        param('Yesterday 1:20 pm', date(2014, 8, 31), time(13, 20)),
+        param('the day before yesterday 16:50', date(2014, 8, 30), time(16, 50)),
+        param('2 Tage 18:50', date(2014, 8, 30), time(18, 50)),
+    ])
+    def test_freshness_date_with_time(self, date_string, date, time):
+        self.given_parser()
+        self.given_date_string(date_string)
+        self.when_date_is_parsed()
+        self.then_date_is(date)
+        self.then_time_is(time)
+
     def given_date_string(self, date_string):
         self.date_string = date_string
 
@@ -284,6 +300,12 @@ class TestFreshnessDateDataParser(BaseTestCase):
             self.result = self.parser.get_date_data(self.date_string)
         except Exception as error:
             self.exception = error
+
+    def then_date_is(self, date):
+        self.assertEqual(date, self.result['date_obj'].date())
+
+    def then_time_is(self, time):
+        self.assertEqual(time, self.result['date_obj'].time())
 
     def then_period_is(self, period):
         self.assertEqual(period, self.result['period'])
