cd97f12c6fee63785061cc6afadff22556a0709f
Waqas <waqas@scrapinghub.com>
2015-05-19 18:46:26 +0500

    Added timezone parsing support for utcoffset format Fixed github issue#45

--- dateparser/timezone_parser.py
+++ dateparser/timezone_parser.py
@@ -4,28 +4,16 @@ from datetime import datetime, timedelta
 
 from dateparser.timezones import timezone_info_list
 
-TIMEZONE_REGEX_PATTERN = r'(\b|\d)%s$'
-TIMEZONE_HHMM_PATTERN = re.compile(r'(?<=\d\d:\d\d)([-+])(\d\d)(\d\d)')
-
 
 def pop_tz_offset_from_string(date_string, as_offset=True):
-    for name, info in _tz_offsets.iteritems():
+    for name, info in _tz_offsets:
         timezone_re = info['regex']
         if timezone_re.search(date_string):
             # \1 = (\b|\d) in TIMEZONE_REGEX_PATTERN
             date_string = timezone_re.sub(r'\1', date_string)
             return date_string, info['offset'] if as_offset else name
     else:
-        for tz_offset in [_get_tz_utc_offset_from_iso_datestamp(date_string)]:
-            return date_string, tz_offset if as_offset else None
-        else:
-            return date_string, None
-
-
-def _get_tz_utc_offset_from_iso_datestamp(date_string):
-    for offset in TIMEZONE_HHMM_PATTERN.findall(date_string):
-        sign, hours, minutes = offset
-        return timedelta(hours=int('%s%s' % (sign, hours)), minutes=int('%s%s' % (sign, minutes)))
+        return date_string, None
 
 
 def convert_to_local_tz(datetime_obj, datetime_tz_offset):
@@ -33,13 +21,25 @@ def convert_to_local_tz(datetime_obj, datetime_tz_offset):
 
 
 def get_tz_offsets():
-    return {
-        tz_info[0]: {
-            'regex': re.compile(TIMEZONE_REGEX_PATTERN % tz_info[0], re.IGNORECASE),
-            'offset': timedelta(seconds=tz_info[1]),
-        }
-        for tz_info in timezone_info_list
-    }
+
+    def get_offset(tz_obj, repl='', replw=''):
+        return (
+            tz_obj[0],
+            {
+                'regex': re.compile(re.sub(repl, replw, regex % tz_obj[0]), re.IGNORECASE),
+                'offset': timedelta(seconds=tz_obj[1])
+            }
+        )
+
+    for tz_info in timezone_info_list:
+        for regex in tz_info['regex_patterns']:
+            for tz_obj in tz_info['timezones']:
+                yield get_offset(tz_obj)
+
+            # alternate patterns
+            for replace, replacewith in tz_info.get('replace', []):
+                for tz_obj in tz_info['timezones']:
+                    yield get_offset(tz_obj, repl=replace, replw=replacewith)
 
 
 def get_local_tz_offset():
@@ -48,5 +48,5 @@ def get_local_tz_offset():
     return offset
 
 
-_tz_offsets = get_tz_offsets()
+_tz_offsets = list(get_tz_offsets())
 local_tz_offset = get_local_tz_offset()
