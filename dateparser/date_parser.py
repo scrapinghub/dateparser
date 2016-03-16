64202d2b02fdcff4468a8ab13d4cf4c77b9727d4
Christopher Arndt <chris@chrisarndt.de>
2016-03-16 15:38:44 +0100

    Handle change in return type of `dateutil.parser.parser._parse()` (fixes #154) and remove version restriction for python-dateutil.

--- dateparser/date_parser.py
+++ dateparser/date_parser.py
@@ -33,6 +33,10 @@ class new_parser(parser.parser):
         # Parse timestr
         res = self._parse(timestr, **kwargs)
 
+        # python-dateutil >= 2.50 changed return type of parser._parse()
+        if isinstance(res, tuple):
+            res = res[0]
+
         if res is None:
             raise ValueError("unknown string format")
 
