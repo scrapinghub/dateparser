75251eaa0bb2deb90fc58e9258e6680e5450f2c6
Ammar Azif <ammar.azif@gmail.com>
2016-08-09 18:22:53 +0800

    Translating arabic time conventions to latin

--- dateparser/calendars/hijri.py
+++ dateparser/calendars/hijri.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from datetime import datetime
 
 from umalqurra.hijri_date import HijriDate
@@ -8,11 +9,23 @@ from dateparser.conf import settings
 
 
 class HijriCalendar(CalendarBase):
+    _time_conventions = {
+        'am': [u"صباحاً"],
+        'pm': [u"مساءً"],
+    }
+
+    def replace_time_conventions(self, source):
+        result = source
+        for latin, arabics in self._time_conventions.items():
+            for arabic in arabics:
+                result = result.replace(arabic, latin)
+        return result
 
     def get_date(self):
         from dateparser.calendars.hijri_parser import hijri_parser
+        translated = self.replace_time_conventions(self.source)
         try:
-            return hijri_parser.parse(self.source, settings)
+            return hijri_parser.parse(translated, settings)
         except ValueError, ex:
             raise ex
             pass
