1ef03a80aadc50a3ac37a46981db32b5059b207c
Waqas Shabir <shabir.waqas@gmail.com>
2016-09-02 20:46:21 +0500

    Fixed unbound local error as explained in is. #219

--- tests/test_date_parser.py
+++ tests/test_date_parser.py
@@ -530,7 +530,8 @@ class TestDateParser(BaseTestCase):
         param('invalid date string', 'Unable to parse: h'),
         param('Aug 7, 2014Aug 7, 2014', 'Unable to parse: Aug'),
         param('24h ago', 'Unable to parse: h'),
-        param('2015-03-17t16:37:51+00:002015-03-17t15:24:37+00:00', 'Unable to parser: 00:002015')
+        param('2015-03-17t16:37:51+00:002015-03-17t15:24:37+00:00', 'Unable to parser: 00:002015'),
+        param('8 enero 2013 martes 7:03 AM EST 8 enero 2013 martes 7:03 AM EST', 'Unable to parse: 8')
     ])
     def test_dates_not_parsed(self, date_string, message):
         self.when_date_is_parsed_by_date_parser(date_string)
