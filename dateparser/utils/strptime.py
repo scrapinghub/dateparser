dbe994b475787daf4bd89c7f79da711562601877
Waqas Shabir <shabir.waqas@gmail.com>
2017-02-06 02:03:52 +0500

    Fix parser accordingly

--- dateparser/utils/strptime.py
+++ dateparser/utils/strptime.py
@@ -1,6 +1,17 @@
 import imp
+import regex as re
 
-def strptime():
+from datetime import datetime
+
+TIME_MATCHER = re.compile(
+    r'(?P<hour>2[0-3]|[0-1]\d|\d):'
+    '(?P<minute>[0-5]\d|\d):'
+    '(?P<second>6[0-1]|[0-5]\d|\d)'
+    '\.(?P<microsecond>[0-9]{1,6})'
+)
+
+
+def patch_strptime():
     """Monkey patching _strptime to avoid problems related with non-english
     locale changes on the system.
 
@@ -8,28 +19,44 @@ def strptime():
     any date since all languages are translated to english dates.
     """
 
-    strptime = imp.load_module(
+    _strptime = imp.load_module(
         'strptime_patched', *imp.find_module('_strptime')
     )
 
-    strptime._getlang = lambda: ('en_US', 'UTF-8')
-    strptime.calendar.day_abbr = [
+    _strptime._getlang = lambda: ('en_US', 'UTF-8')
+    _strptime.calendar.day_abbr = [
         'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'
     ]
-    strptime.calendar.day_name = [
+    _strptime.calendar.day_name = [
         'monday', 'tuesday', 'wednesday', 'thursday',
         'friday', 'saturday', 'sunday'
     ]
-    strptime.calendar.month_abbr = [
+    _strptime.calendar.month_abbr = [
         '', 'jan', 'feb', 'mar', 'apr', 'may', 'jun',
         'jul', 'aug', 'sep', 'oct', 'nov', 'dec'
     ]
-    strptime.calendar.month_full = [
+    _strptime.calendar.month_full = [
         '', 'january', 'february', 'march', 'april',
         'may', 'june', 'july', 'august', 'september',
         'october', 'november', 'december'
     ]
 
-    return strptime._strptime_time
+    return _strptime._strptime_time
 
-__strptime = strptime()
+
+__strptime = patch_strptime()
+
+
+def strptime(date_string, format):
+    obj = datetime(*__strptime(date_string, format)[:-3])
+
+    if '%f' in format:
+        try:
+            match_groups = TIME_MATCHER.match(date_string).groupdict()
+            ms = match_groups['microsecond']
+            ms = ms + ((6 - len(ms)) * '0')
+            obj = obj.replace(microsecond=int(ms))
+        except AttributeError:
+            pass
+
+    return obj
