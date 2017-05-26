6fb993899fc47e7553eea62c5e24e3a531b8bce3
sarthak5 <sarthakmadaan5121995@gmail.com>
2017-05-26 23:16:50 +0530

    added more testcases

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -12,10 +12,18 @@ class TestLanguageDataLoader(BaseTestCase):
         cls.data_loader._data = {}
 
     @parameterized.expand([
-        param(languages=['es','fr','en'], loaded_languages=['en','es','fr'], returned_languages=['en','es','fr']),
-        param(languages=['ar','he'], loaded_languages=['en','ar','es','fr','he'], returned_languages=['ar','he']),
-        param(languages=['tl','zh','uk'], loaded_languages=['en','ar','es','fr','he','tl','uk','zh'], returned_languages=['tl','uk','zh']),
-        param(languages=['fr','zh','he','ru'], loaded_languages=['en','ar','es','fr','he','ru','tl','uk','zh'], returned_languages=['fr','he','ru','zh']),
+        param(languages=['es','fr','en'], loaded_languages=['en','es','fr'],
+              returned_languages=['en','es','fr']),
+        param(languages=['ar','he'], loaded_languages=['en','ar','es','fr','he'],
+              returned_languages=['ar','he']),
+        param(languages=['tl','zh','uk'], loaded_languages=['en','ar','es','fr','he','tl','uk','zh'],
+              returned_languages=['tl','uk','zh']),
+        param(languages=['fr','zh','he','ru'], loaded_languages=['en','ar','es','fr','he','ru','tl','uk','zh'],
+              returned_languages=['fr','he','ru','zh']),
+        param(languages=['en','tl'], loaded_languages=['en','ar','es','fr','he','ru','tl','uk','zh'],
+              returned_languages=['en','tl']),
+        param(languages=['it'], loaded_languages=['en','ar','es','fr','he','it','ru','tl','uk','zh'],
+              returned_languages=['it']),
     ])
     def test_loading(self, languages, loaded_languages, returned_languages):
         self.load_data(languages)
