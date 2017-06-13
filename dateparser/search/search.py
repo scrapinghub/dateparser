2061e02afd39d4104bd8123d6f1427429271a275
eszakharova <1583253@gmail.com>
2017-06-14 01:22:29 +0300

    [WIP] search with translate

--- dateparser/search/search.py
+++ dateparser/search/search.py
@@ -17,12 +17,3 @@ class ExactLanguageSearch:
         translated, original = self.language.translate_search(text, settings=Settings())
         return translated, original
 
-el = ExactLanguageSearch()
-# print(el.search('ru', '5 января 2005 все отдыхали, 6 января все отдыхали, '
-#                 '7 января все отдыхали, в понедельник пришлось выйти на работу'))
-print(el.search('en', 'Game 1 is July 12, 2017. Game 2 on July 13th. Game 3 on July 15th'))
-print(el.search('en', 'I will meet you tomorrow at noon'))
-print(el.search('ru', 'July 3, 2017 - August 1st'))
-print(el.search('en', 'in a minute'))
-print(el.search('en', 'July 13th, July 14th'))
-print(el.search('en', 'July 13th. July 14th'))
