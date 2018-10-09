28e6c721a11eba470f9cac98114e76b13d226291
Renne Rocha <renne@scrapinghub.com>
2018-10-09 15:26:46 -0300

    Included new parameter to search_dates function docs

--- dateparser/search/__init__.py
+++ dateparser/search/__init__.py
@@ -11,27 +11,36 @@ def search_dates(text, languages=None, settings=None, add_detected_language=Fals
         :param text:
             A string in a natural language which may contain date and/or time expressions.
         :type text: str|unicode
+
         :param languages:
             A list of two letters language codes.e.g. ['en', 'es']. If languages are given, it will
             not attempt to detect the language.
         :type languages: list
+
         :param settings:
                Configure customized behavior using settings defined in :mod:`dateparser.conf.Settings`.
         :type settings: dict
+
         :param add_detected_language:
                Indicates if we want the detected language returned in the tuple.
-        :type settings: bool
+        :type add_detected_language: bool
 
         :return: Returns list of tuples containing:
                  substrings representing date and/or time, corresponding :mod:`datetime.datetime`
-                 object and detected language in parameter add_detected_language set to True.
+                 object and detected language if *add_detected_language* is True.
                  Returns None if no dates that can be parsed are found.
         :rtype: list
         :raises: ValueError - Unknown Language
 
+        >>> from dateparse.search import search_dates
         >>> search_dates('The first artificial Earth satellite was launched on 4 October 1957.')
         [('on 4 October 1957', datetime.datetime(1957, 10, 4, 0, 0))]
-        >>> search_dates('on 4 October 1957', add_detected_language=True)
+
+        >>> search_dates("The client arrived to the office for the first time in March 3rd, 2004 and got serviced, after a couple of months, on May 6th 2004, the customer returned indicating a defect on the part")
+        [(u'in March 3rd, 2004 and', datetime.datetime(2004, 3, 3, 0, 0)),
+        (u'on May 6th 2004', datetime.datetime(2004, 5, 6, 0, 0))]
+
+        >>> search_dates('The first artificial Earth satellite was launched on 4 October 1957.')
         [('on 4 October 1957', datetime.datetime(1957, 10, 4, 0, 0), 'en')]
 
         """
