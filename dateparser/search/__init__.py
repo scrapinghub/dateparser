ec57c4511b3526e43643dc3ec971cc543d02f0d1
Renne Rocha <renne@scrapinghub.com>
2018-10-09 15:10:34 -0300

    Included new argument to allow search function to return the detected language

--- dateparser/search/__init__.py
+++ dateparser/search/__init__.py
@@ -5,31 +5,41 @@ from dateparser.search.search import DateSearchWithDetection
 _search_with_detection = DateSearchWithDetection()
 
 
-def search_dates(text, languages=None, settings=None):
+def search_dates(text, languages=None, settings=None, add_detected_language=False):
     """Find all substrings of the given string which represent date and/or time and parse them.
 
         :param text:
             A string in a natural language which may contain date and/or time expressions.
         :type text: str|unicode
         :param languages:
-            A list of two letters language codes.e.g. ['en', 'es']. If languages are given, it will not attempt
-            to detect the language.
+            A list of two letters language codes.e.g. ['en', 'es']. If languages are given, it will
+            not attempt to detect the language.
         :type languages: list
         :param settings:
                Configure customized behavior using settings defined in :mod:`dateparser.conf.Settings`.
         :type settings: dict
+        :param add_detected_language:
+               Indicates if we want the detected language returned in the tuple.
+        :type settings: bool
 
-
-        :return: Returns list of tuples containing pairs:
-                 substrings representing date and/or time and corresponding :mod:`datetime.datetime` object.
+        :return: Returns list of tuples containing:
+                 substrings representing date and/or time, corresponding :mod:`datetime.datetime`
+                 object and detected language in parameter add_detected_language set to True.
                  Returns None if no dates that can be parsed are found.
         :rtype: list
         :raises: ValueError - Unknown Language
 
         >>> search_dates('The first artificial Earth satellite was launched on 4 October 1957.')
         [('on 4 October 1957', datetime.datetime(1957, 10, 4, 0, 0))]
+        >>> search_dates('on 4 October 1957', add_detected_language=True)
+        [('on 4 October 1957', datetime.datetime(1957, 10, 4, 0, 0), 'en')]
 
         """
-    result = _search_with_detection.search_dates(text=text, languages=languages, settings=settings)
-    if result['Dates']:
-        return result['Dates']
+    result = _search_with_detection.search_dates(
+        text=text, languages=languages, settings=settings
+    )
+    language, dates = result.get('Language'), result.get('Dates')
+    if dates:
+        if add_detected_language:
+            dates = [(*date, language) for date in dates]
+        return dates
