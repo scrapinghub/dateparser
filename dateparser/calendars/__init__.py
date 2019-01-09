194ad7b7c3cb093d81f3298b26446f8d5882c263
Andial <luisandiamamani@gmail.com>
2019-01-09 00:49:34 -0500

    add test

--- dateparser/calendars/__init__.py
+++ dateparser/calendars/__init__.py
@@ -24,7 +24,8 @@ class CalendarBase(object):
             date_obj, period = self.parser.parse(self.source, settings)
             return {'date_obj': date_obj, 'period': period}
         except ValueError:
-            pass
+            import traceback
+            traceback.print_exc()
 
 
 class non_gregorian_parser(_parser):
@@ -86,7 +87,7 @@ class non_gregorian_parser(_parser):
             if not(year >= 1356 and year <= 1501):
                 raise ValueError('The year must be greater than or equal to 1356 and less or equal 1501')
             if (year == 1501 and month==1 and day<31):
-                raise ValueError('The date must be less than 1501-01-30')
+                raise ValueError('The date must be less or equal than 1501-01-30')
         if (
             not(0 < day <= self.calendar_converter.month_length(year, month)) and
             not(self._token_day or hasattr(self, '_token_weekday'))
--- tests/test_hijri.py
+++ tests/test_hijri.py
@@ -54,3 +54,16 @@ class TestHijriParser(BaseTestCase):
         self.when_date_is_given(dt_string, date_formats, languages)
         self.then_parsed_datetime_is(dt_obj)
         settings.DATE_ORDER = 'MDY'
+
+    @parameterized.expand([
+        param(dt_string="14-09-1502",error='The year must be greater than or equal to 1356 and less or equal 1501'),
+        param(dt_string="30-01-1501",error='The date must be less than 1501-01-30'),
+    ])
+    def test_datetime_out_of_range(self, dt_string,error,
+                            date_formats=None, languages=None):
+        from dateparser.conf import settings
+        settings.DATE_ORDER = 'DMY'
+        try:
+            self.when_date_is_given(dt_string, date_formats, languages)
+        except ValueError:
+            pass
