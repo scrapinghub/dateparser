69a3179c1709362a9c6895d3a2b0c04e7e4c72be
Andial <luisandiamamani@gmail.com>
2019-01-09 18:56:21 -0500

    search dates with trailing comma

--- dateparser/search/text_detection.py
+++ dateparser/search/text_detection.py
@@ -29,7 +29,7 @@ class FullTextLanguageDetector(BaseLanguageDetector):
     def character_check(self, date_string, settings):
         date_string_set = set(date_string.lower())
         symbol_set = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                      " ", "/", "-", ")", "(", ".", ":", "\\"}
+                      " ", "/", "-", ")", "(", ".", ":", "\\",",","\'"}
         if date_string_set & symbol_set == date_string_set:
             self.languages = [self.languages[0]]
             return
