b34585dee59a3539843a7c0a233139f6084693b5
Unknown <watchful@watchful.gr>
2019-06-18 23:36:23 -0700

    Auto stash before revert of "Checks didn't like that. Let's try this way"

--- tests/test_freshness_date_parser.py
+++ tests/test_freshness_date_parser.py
@@ -1563,30 +1563,6 @@ class TestFreshnessDateDataParser(BaseTestCase):
         self.then_date_is(date)
         self.then_time_is(time)
 
-    @parameterized.expand([
-        param('3 days', date(2010, 6, 1), time(13, 15)),
-        param('2 years', date(2008, 6, 4), time(13, 15)),
-    ])
-    def test_freshness_date_with_relative_base_past(self, date_string, date, time):
-        self.given_parser(settings={'PREFER_DATES_FROM': 'past',
-                          'RELATIVE_BASE': datetime(2010, 6, 4, 13, 15)})
-        self.given_date_string(date_string)
-        self.when_date_is_parsed()
-        self.then_date_is(date)
-        self.then_time_is(time)
-
-    @parameterized.expand([
-        param('3 days', date(2010, 6, 7), time(13, 15)),
-        param('2 years', date(2012, 6, 4), time(13, 15)),
-    ])
-    def test_freshness_date_with_relative_base_future(self, date_string, date, time):
-        self.given_parser(settings={'PREFER_DATES_FROM': 'future',
-                          'RELATIVE_BASE': datetime(2010, 6, 4, 13, 15)})
-        self.given_date_string(date_string)
-        self.when_date_is_parsed()
-        self.then_date_is(date)
-        self.then_time_is(time)
-
     def given_date_string(self, date_string):
         self.date_string = date_string
 
