8ae71da2dd58cc9f2d6fcf44a21d852d63a338d0
jayongg <jayongg_2001@hotmail.com>
2019-12-10 00:10:02 -0800

    let date parsing respect settings

--- dateparser/languages/locale.py
+++ dateparser/languages/locale.py
@@ -142,7 +142,7 @@ class Locale(object):
             else:
                 if word in dictionary:
                     date_string_tokens[i] = dictionary[word] or ''
-        if "in" in date_string_tokens or 'future' in self.settings.PREFER_DATES_FROM:
+        if "in" in date_string_tokens:
             date_string_tokens = self._clear_future_words(date_string_tokens)
 
         return self._join(list(filter(bool, date_string_tokens)),
