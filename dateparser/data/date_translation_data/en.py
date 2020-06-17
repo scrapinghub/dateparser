4034cea9a8510742232335c04507873b88fd00ab
arnav <arnav.kapoor@research.iiit.ac.in>
2020-06-17 17:32:47 +0530

    removing hard-coded numerals for english

--- dateparser/data/date_translation_data/en.py
+++ dateparser/data/date_translation_data/en.py
@@ -782,42 +782,6 @@ info = {
         },
         {
             "less than 1 minute ago": "45 second ago"
-        },
-        {
-            "one": "1"
-        },
-        {
-            "two": "2"
-        },
-        {
-            "three": "3"
-        },
-        {
-            "four": "4"
-        },
-        {
-            "five": "5"
-        },
-        {
-            "six": "6"
-        },
-        {
-            "seven": "7"
-        },
-        {
-            "eight": "8"
-        },
-        {
-            "nine": "9"
-        },
-        {
-            "ten": "10"
-        },
-        {
-            "eleven": "11"
-        },
-        {
-            "twelve": "12"
         }
     ]
 }
