5906ae49eb7d85333b134a87b24153792f92af1c
Adrián Chaves <adrian@chaves.io>
2020-08-12 09:25:52 +0200

    Use more explicit expectations

--- tests/test_input.py
+++ tests/test_input.py
@@ -4,8 +4,8 @@ from tests import BaseTestCase
 class InputTest(BaseTestCase):
     def empty_input_test(self):
         result = parse('')
-        self.assertFalse(result)
+        self.assertEqual(result, None)
 
     def whitespace_input_test(self):
         result = parse(' \t\n\r\f\v\x00')
-        self.assertFalse(result)
+        self.assertEqual(result, None)
