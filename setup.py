5e415c3b8ec3f4d00827285577b7c49df571e8cd
arnavkapoor <arnav.kapoor@research.iiit.ac.in>
2020-08-26 12:45:47 +0530

    minimum v0.2.1 of number-parser needed

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         'python-dateutil',
         'pytz',
-        'number-parser',
+        'number-parser >= 0.2.1',
         # https://bitbucket.org/mrabarnett/mrab-regex/issues/314/import-error-no-module-named
         'regex !=2019.02.19; platform.python_implementation != "PyPy"',
         # temporary workaround for: https://github.com/scrapinghub/dateparser/issues/565
