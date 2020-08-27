6798f7395a6b163fbffdca6674735e662bcc6d48
Kishan Mehta <kishan@scrapinghub.com>
2020-08-27 17:52:32 +0530

    Removed line 'Programming Language :: Python :: Implementation :: PyPy'.

--- setup.py
+++ setup.py
@@ -55,6 +55,5 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
     ],
 )
