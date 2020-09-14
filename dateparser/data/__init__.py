4f01811f114a4c172821b8017913a1f388ae53c7
Marc Hernández <noviluni@gmail.com>
2020-09-14 23:08:47 +0200

    remove numeral translation_data (#782)
    * remove numeral translation_data
    
    * remove json files for numeral data
    
    * remove py files for numeral data

--- dateparser/data/__init__.py
+++ dateparser/data/__init__.py
@@ -1,2 +1,2 @@
-from dateparser.data import date_translation_data, numeral_translation_data
+from dateparser.data import date_translation_data
 from .languages_info import language_order, language_locale_dict
