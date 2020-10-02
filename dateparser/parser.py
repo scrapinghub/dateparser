cdccfa54e5b4a9201c38b8258e7176f8e64c40fd
Marc <noviluni@gmail.com>
2020-10-02 09:29:33 +0200

    remove unused variable

--- dateparser/parser.py
+++ dateparser/parser.py
@@ -264,8 +264,10 @@ class _parser:
 
                 try:
                     microsecond = MICROSECOND.search(self.filtered_tokens[index + 1][0]).group()
-                    token.index(":")  # raise ValueError if ':' can't be found
-                    _is_after_period = self.tokens[self.tokens.index((token, 0)) + 1][0].index('.')
+                    # Is after time token? raise ValueError if ':' can't be found:
+                    token.index(":")
+                    # Is after period? raise ValueError if '.' can't be found:
+                    self.tokens[self.tokens.index((token, 0)) + 1][0].index('.')
                 except:
                     microsecond = None
 
