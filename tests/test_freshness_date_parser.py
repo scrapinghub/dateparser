bf3578062840930a3435cc0131af9e7c91937b4b
NEERAJ ADITYANANTH POLAMPALLI <65017645+NEERAJAP2001@users.noreply.github.com>
2020-10-15 20:07:30 +0530

    Update test_freshness_date_parser.py

--- tests/test_freshness_date_parser.py
+++ tests/test_freshness_date_parser.py
@@ -34,6 +34,13 @@ class TestFreshnessDateDataParser(BaseTestCase):
 
     @parameterized.expand([
         # English dates
+        param("1 century", ago={'years': 100}, period='year'),
+        param("1 century 2 years", ago={'years': 112}, period='year'),
+        param("1 century 12 months", ago={'years': 100, 'months': 12}, period='month'),
+        param("1 century and 11 months", ago={'years': 100, 'months': 11}, period='month'),
+        param("last century", ago={'years': 100}, period='year'),
+        param("a century ago", ago={'years': 100}, period='year'),
+        param("100 centuries", ago={'years': 10000}, period='year'),
         param("1 decade", ago={'years': 10}, period='year'),
         param("1 decade 2 years", ago={'years': 12}, period='year'),
         param("1 decade 12 months", ago={'years': 10, 'months': 12}, period='month'),
