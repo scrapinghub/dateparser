4ed488bb9e889a219aa051fe41ac85b0b962c51e
mirceachira <chira.mircea.darius@gmail.com>
2020-10-31 11:06:41 +0200

    fix language order duplicates

--- dateparser_scripts/order_languages.py
+++ dateparser_scripts/order_languages.py
@@ -12,7 +12,12 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 avoid_languages = ['cu', 'kkj', 'nds', 'prg', 'tk', 'vai', 'vai-Latn', 'vai-Vaii', 'vo']
 
 # Order from https://w3techs.com/technologies/overview/content_language
-most_common_locales = ['en', 'ru', 'es', 'tr', 'fa', 'fr', 'de', 'ja', 'pt', 'vi', 'zh', 'ar', 'it', 'pl', 'in', 'el', 'nl', 'ko', 'th', 'iw', 'uk', 'cs', 'sv', 'ro', 'hu', 'da', 'sr', 'sk', 'fi', 'bg', 'hr', 'lt', 'hi', 'no_NO', 'sl', 'no', 'et', 'lv']
+# Last updated on 30.10.2020
+most_common_locales = [
+    'en', 'ru', 'es', 'tr', 'fa', 'fr', 'de', 'ja', 'pt', 'vi', 'zh', 'ar', 'it', 'pl', 'in', 'el',
+    'nl', 'ko', 'th', 'iw', 'uk', 'cs', 'sv', 'ro', 'hu', 'da', 'sr', 'sk', 'fi', 'bg', 'hr', 'lt',
+    'hi', 'no_NO', 'sl', 'no', 'et', 'lv'
+]
 
 
 def _get_language_locale_dict():
@@ -53,8 +58,18 @@ def _get_language_order(language_locale_dict):
         except:
             pass
 
-    language_order = most_common_locales + sorted(language_population_dict.keys(),
-                            key=lambda x: (language_population_dict[x], x), reverse=True)
+    language_order_with_duplicates = (
+        most_common_locales +
+        sorted(
+            language_population_dict.keys(),
+            key=lambda x: (language_population_dict[x], x), reverse=True
+        )
+    )
+    language_order = sorted(
+        set(language_order_with_duplicates),
+        key=lambda x: language_order_with_duplicates.index(x)
+    )
+
     for index in range(0, len(language_order)):
         language_order[index] = re.sub(r'_', r'-', language_order[index])
 
