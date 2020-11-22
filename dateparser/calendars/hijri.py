2ace80441f7dbcfd3b8ca35000981f3fb9f370c3
Marc <noviluni@gmail.com>
2020-11-22 21:25:08 +0100

    wip

--- dateparser/calendars/hijri.py
+++ dateparser/calendars/hijri.py
@@ -4,3 +4,9 @@ from dateparser.calendars.hijri_parser import hijri_parser
 
 class HijriCalendar(CalendarBase):
     parser = hijri_parser
+
+    def get_date(self):
+        try:
+            date_data = super().get_date()
+        except OverflowError:
+            return None
