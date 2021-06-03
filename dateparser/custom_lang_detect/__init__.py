499a29d36ca46e083138f6ee47722a36eaf10b3d
Gavish <gavishpoddar@hotmail.com>
2021-06-03 21:30:33 +0000

    custom language parser updates

--- dateparser/custom_lang_detect/__init__.py
+++ dateparser/custom_lang_detect/__init__.py
@@ -1,11 +1,19 @@
 import importlib
 
+loaded_parser = None
+language_parser = None
 
 def language_parser_m(paser_module="fast_text"):
+  global loaded_parser
+  global language_parser
+
   try:
-    importlib.import_module("dateparser.custom_lang_detect." + paser_module)
-    print("custom loader init")
+    if loaded_parser == None:
+      paser_module = importlib.import_module("dateparser.custom_lang_detect." + paser_module)
+      language_parser = paser_module.language_parser
+      print("custom loader init")
   except:
     print("Requested model not found", paser_module)
 
-  return  fast_text.language_parser
+  loaded_parser = paser_module
+  return  language_parser
