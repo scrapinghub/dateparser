7139c79107ef4402071fd2386fdedbc36b1e295b
Gavish <gavishpoddar@hotmail.com>
2021-07-30 14:47:41 +0000

    Updating langdetect.py `_get_language_probablities`

--- dateparser/custom_language_detection/langdetect.py
+++ dateparser/custom_language_detection/langdetect.py
@@ -2,7 +2,7 @@ from langdetect.detector_factory import DetectorFactory, PROFILES_DIRECTORY
 import langdetect
 
 
-# The below Factory is set to prevent setting global state of the library
+# The below _Factory is set to prevent setting global state of the library
 # but still get consistent results.
 # Refer - https://github.com/Mimino666/langdetect
 
@@ -10,15 +10,15 @@ class Factory:
     data = None
 
 
-def init_factory():
+def _init_factory():
     if Factory.data is None:
         Factory.data = DetectorFactory()
         Factory.data.load_profile(PROFILES_DIRECTORY)
         Factory.data.seed = 0
 
 
-def detect_langs(text):
-    init_factory()
+def _get_language_probablities(text):
+    _init_factory()
     detector = Factory.data.create()
     detector.append(text)
     return detector.get_probabilities()
@@ -27,7 +27,7 @@ def detect_langs(text):
 def detect_languages(text, confidence_threshold):
     language_codes = []
     try:
-        parser_data = detect_langs(text)
+        parser_data = _get_language_probablities(text)
         for langauge_candidate in parser_data:
             if langauge_candidate.prob > confidence_threshold:
                 language_codes.append(langauge_candidate.lang)
