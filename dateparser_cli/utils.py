1ba3dc2c55f032ba62f6c638640e5c6afa29bbe0
Gavish <gavishpoddar@hotmail.com>
2021-08-17 12:59:12 +0000

    removing env variable

--- dateparser_cli/utils.py
+++ dateparser_cli/utils.py
@@ -2,18 +2,13 @@ import sys
 import os
 from pathlib import Path
 
-DEFAULT_UNIX_CACHE_DIR = os.environ.get("DATEPARSER_MODELS_CACHE_DIR", '~/.cache')
+DEFAULT_DIR_NAME = 'dateparser_models'
+DEFAULT_UNIX_CACHE_DIR = '~/.cache'
 
 if sys.version_info < (3, 6):  # python 3.5 compatibility
-    DEFAULT_WINDOWS_CACHE_DIR = os.environ.get(
-        "DATEPARSER_MODELS_CACHE_DIR", os.path.join(str(Path.home()), "AppData", "Roaming")
-    )
+    DEFAULT_WINDOWS_CACHE_DIR = os.path.join(str(Path.home()), "AppData", "Roaming")
 else:
-    DEFAULT_WINDOWS_CACHE_DIR = os.environ.get(
-        "DATEPARSER_MODELS_CACHE_DIR", os.path.join(Path.home(), "AppData", "Roaming")
-    )
-
-DEFAULT_DIR_NAME = os.environ.get("DATEPARSER_MODELS_DIR_NAME", 'dateparser_models')
+    DEFAULT_WINDOWS_CACHE_DIR = os.path.join(Path.home(), "AppData", "Roaming")
 
 
 if sys.platform.startswith('win'):
