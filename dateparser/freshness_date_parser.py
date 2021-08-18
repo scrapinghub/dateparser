9a4707ec1d6b3ce830f58a5fea4e5576505d8f9e
Marc <noviluni@gmail.com>
2021-08-18 19:16:15 +0200

    sort frshness_date_parser.py imports

--- dateparser/freshness_date_parser.py
+++ dateparser/freshness_date_parser.py
@@ -1,14 +1,13 @@
-import regex as re
 from datetime import datetime
 from datetime import time
-from tzlocal import get_localzone
 
+import regex as re
 from dateutil.relativedelta import relativedelta
+from tzlocal import get_localzone
 
 from dateparser.utils import apply_timezone, localize_timezone, strip_braces
 from .parser import time_parser
 from .timezone_parser import pop_tz_offset_from_string
-
 
 _UNITS = r'decade|year|month|week|day|hour|minute|second'
 PATTERN = re.compile(r'(\d+)\s*(%s)\b' % _UNITS, re.I | re.S | re.U)
