0c596acb5fb955888d164e8f72520afb6b59d84a
asadurski <artur@scrapinghub.com>
2022-03-17 13:23:20 +0100

    pin regex version
    https://github.com/scrapinghub/dateparser/issues/1045

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'python-dateutil',
         'pytz',
         # https://bitbucket.org/mrabarnett/mrab-regex/issues/314/import-error-no-module-named
-        'regex !=2019.02.19,!=2021.8.27',
+        'regex !=2019.02.19,!=2021.8.27,<2022.3.15',
         'tzlocal',
     ],
     entry_points={
