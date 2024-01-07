d81b984563d9c2b399c6ccce423011c6d8e86d05
bcapuano <bcapuano@asu.edu>
2024-01-06 19:13:47 -0500

    Fixed ClusterFuzz build error by adding dateparserr.data as a binary dependency in build.sh pyinstaller

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -3,6 +3,6 @@ pip3 install .
 
 # Build fuzzers in $OUT
 for fuzzer in $(find fuzzing -name '*_fuzzer.py');do
-  compile_python_fuzzer "$fuzzer"
+  compile_python_fuzzer "$fuzzer" --add-binary="dateparser/data:dateparser/data"
 done
 zip -q $OUT/dateparser_fuzzer_seed_corpus.zip $SRC/corpus/*
